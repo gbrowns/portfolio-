37c606395b9dab7557516e14cb4a2476ac63044c
Godwin254 <godwinbee15@gmail.com>
2022-06-19 09:38:54 +0300

    update fetch request from api

--- src/app.mjs
+++ src/app.mjs
@@ -4,21 +4,7 @@ const url = 'https://jsonplaceholder.typicode.com/posts'
 const path = '../data.data.json';
 //console.log(url);
 
-async function fetchData(link){
-
-    try{
-        const res = await fetch(link);
-
-        let data = await res.json();
-
-        //console.log(data);
-
-    }catch(e){
-        console.log(e);
-    }
-
-    //jquery
-    $.getJSON(path, data => console.log(data));
-}
-
-fetchData(url);
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
